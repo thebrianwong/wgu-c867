4aa5338aba1a3d0c1ec22b3efa9c5b5e4380698a
Brian Wong <brianw7218@gmail.com>
2023-08-14 16:39:38 -0700

    Remove index data member and add destructor

--- roster.h
+++ roster.h
@@ -7,18 +7,14 @@
 class Roster
 {
 public: // Requirement E3
-  // Roster();
+  ~Roster();
   void add(string studentID, string firstName, string lastName, string emailAddress, int age, int daysInCourse1, int daysInCourse2, int daysInCourse3, DegreeProgram degreeProgram);
   void remove(string studentID);
   void printAll();
   void printAverageDaysInCourse(string studentID);
   void printInvalidEmails();
   void printByDegreeProgram(DegreeProgram degreeProgram);
-
-public: // Requirement E1
   Student *classRosterArray[5];
-  int currentArrayIndex = 0;
-  // Student classRosterArray[5];
 };
 
 #endif
